4f9b0155266f00bdd562b42b11263bbaab726ad5
Jeremy Eissler <jeissler0@gmail.com>
2025-03-20 19:29:02 -0500

    replace nuxt tailwind plugin w/ vite - fixes v4 issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: [
-    "@nuxt/eslint",
-    "@nuxtjs/tailwindcss",
-    "nuxt-svgo",
-    "@nuxt/content",
-  ],
+  css: ["~/styles/main.pcss"],
+  modules: ["@nuxt/eslint", "nuxt-svgo", "@nuxt/content"],
   svgo: {
     autoImportPath: false,
     defaultImport: "component",
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
   app: {
     head: {
